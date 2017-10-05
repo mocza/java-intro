f62a18606fe6c6d347caba2b838d1914453b5be4
jasper-mocha <jasper.g.mocha@gmail.com>
2017-10-05 21:38:23 +0100

    structure test case

--- src/test/java/maven1/MyClassTest1.java
+++ src/test/java/maven1/MyClassTest1.java
@@ -8,10 +8,13 @@ public class MyClassTest1 {
 
 	@Test
 	public void test1() {
+		// given
 		MyClass myClass = new MyClass();
 		
+		// when
 		String output = myClass.getName();
 		
+		// then
 		assertEquals("hi", output);		
 	}
 	
